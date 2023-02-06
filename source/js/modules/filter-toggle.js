be47c8b7c7fca2d9c4ee127a9d9350d80ed31dbd
unknown <bulatsrc@gmail.com>
2023-02-07 00:22:52 +0500

    updates markup

--- source/js/modules/filter-toggle.js
+++ source/js/modules/filter-toggle.js
@@ -1,12 +1,13 @@
 const filterToggle = () => {
-    const filters = document.querySelectorAll('.filter');
+    const filters = document.querySelectorAll('.dropdown-menu');
     if (filters[0]) {
         filters.forEach((filter) => {
-            const filterButton = filter.querySelector('.filter_dropdown');
-            const filterContent = filter.querySelector('.filter_content');
+            const filterButton = filter.querySelector('.dropdown-menu_dropdown');
+            const filterContent = filter.querySelector('.dropdown-menu_content');
             const closeButton = filter.querySelector('.js-filter-close');
             filterButton.addEventListener('click', () => {
                 filterContent.classList.toggle('active');
+                filterButton.classList.toggle('active');
             });
             closeButton.addEventListener('click', () => {
                 filterContent.classList.remove('active');
